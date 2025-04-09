d0f51c1adbfb3266b28c19c7bf8078e3d467a878
JadenLeung <j28leung@uwaterloo.ca>
2025-04-09 00:58:48 -0400

    fixed starter

--- backend/code/Q1/main.c
+++ backend/code/Q1/main.c
@@ -1,6 +1,7 @@
 
 int main(void) {
     int a = 0;
+    int b = 0;
     scanf("%d", &a);
     scanf("%d", &b);
     printf("%d\n", add(a, b));
--- backend/main.c
+++ backend/main.c
@@ -4,13 +4,15 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdbool.h>
-// last_digit(a): Gets the first digit of a
-int last_digit(int a) {
-    // Your code here
+// add(a, b): Returns the sum of a and b
+int add(int a, int b) {
+    return a + b;
 }
 
 int main(void) {
     int a = 0;
+    int b = 0;
     scanf("%d", &a);
-    printf("%d\n", last_digit(a));
+    scanf("%d", &b);
+    printf("%d\n", add(a, b));
 }
