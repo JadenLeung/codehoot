4035f6c81d48b8611b3630c5294b4df68719f98d
JadenLeung <jaden.leung@gmail.com>
2025-08-01 18:22:37 -0400

    added phooto

--- frontend/my-app/src/Titlecode.js
+++ frontend/my-app/src/Titlecode.js
@@ -172,7 +172,7 @@ function Titlecode({ setOutput, mode, setMode, buttonText, placeholderText, avat
     }
   }, [mode]);
 
-  const picList = ["C", "c++", "python", "racket", "java", "scratch", "html", "miles", "jaden", "leo", "darius"]
+  const picList = ["C", "c++", "python", "racket", "java", "scratch", "html", "miles", "jaden", "carter", "leo", "darius"]
 
   if (mode != "results") {
     return (
