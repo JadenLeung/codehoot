5b7908320b67a7f256e66c3849a58b7228cd8c57
JadenLeung <jaden.leung@gmail.com>
2025-10-30 00:05:18 -0400

    bruh

---
+++ backend/code/char_limit/main.c
@@ -0,0 +1,17 @@
+int main(void) {
+    // Testing harness you don't have to worry about
+    char str[1000];
+    int line = 1;
+    while (fgets(str, sizeof(str), stdin) != NULL) {
+        int len = strlen(str);
+        if (len > 0 && str[len - 1] == '\n') {
+            str[len - 1] = '\0'; // Remove newline
+        }
+        if (!char_limit(str)) {
+            printf("Exceeded character limit on line %d\n", line);
+        }
+        ++line;
+    }
+    printf("End of input.\n");
+    return 0;
+}
---
+++ backend/code/char_limit/startcode.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+const int MAX_LEN = 80;
+const char *badspaces[6] = {"if(", "for(", "while(", "){", "else{", "}else"};
+const int BADSPACES_LEN = 6;
+const char *types[4] = {"int", "char", "bool", "struct"};
+const int TYPES_LEN = 4;
+
+// char_limit(str): Returns true if str is <= the character limit MAX_LEN
+// requires: str is a valid string
+bool char_limit(const char *str) {
+    return true;
+}
