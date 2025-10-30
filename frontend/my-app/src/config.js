dc9c68ac36bf1c33bb69496beabf1c1c24080f45
JadenLeung <jaden.leung@gmail.com>
2025-10-29 23:11:56 -0400

    ez

--- frontend/my-app/src/config.js
+++ frontend/my-app/src/config.js
@@ -4,19 +4,20 @@ const config = {
     // flask: "http://127.0.0.1:8080",
     port: 5005,
     showRank: 0.2, // Decimal from 0-1. If 0.4, the top 40% of people get to see their scores.
-    submitCooldown: 20,
+    submitCooldown: 0,
     namelength: 15,
     timeout: 3, // # seconds until clang times out
     websocket: 'https://api.virtual-cube.net:3001/',
     // websocket: 'http://localhost:3001',
     qdata: {
-        Q1: { time: 150, name: "linked_sum", testcases: 6 },
-        Q2: { time: 500, name: "linked_middle", testcases: 7 },
-        Q3: { time: 500, name: "bsearch", testcases: 5 },
-        Q4: { time: 500, name: "bst_height", testcases: 6 },
-        Q5: { time: 500, name: "is_bst", testcases: 9 },
-        Q6: { time: 800, name: "least_letter", testcases: 7 },
-        Q7: { time: 600, name: "limited_calculator", testcases: 8 },
+        Q1: { time: 600, name: "linalg", testcases: 7 },
+        Q2: { time: 150, name: "linked_sum", testcases: 6 },
+        Q3: { time: 500, name: "linked_middle", testcases: 7 },
+        Q4: { time: 500, name: "bsearch", testcases: 5 },
+        Q5: { time: 500, name: "bst_height", testcases: 6 },
+        Q6: { time: 500, name: "is_bst", testcases: 9 },
+        Q7: { time: 800, name: "least_letter", testcases: 7 },
+        Q8: { time: 600, name: "limited_calculator", testcases: 8 },
     },
 
     inspiration: ["In modularity, you want low coupling, high cohesion", "You cannot directly dereference a void pointer.", 
